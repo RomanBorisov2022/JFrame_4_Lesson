acd12f53bf5a665f9ea013b3c42126ee73d3eb59
Roman Borisov <boriso-rom@yandex.ru>
2023-12-22 14:01:02 +0300

    created class Employee

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,15 @@
+package org.example;
+
+class Employee {
+    int employeeId;
+    String phoneNumber;
+    String name;
+    int experience;
+
+    public Employee(int employeeId, String phoneNumber, String name, int experience) {
+        this.employeeId = employeeId;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.experience = experience;
+    }
+}
