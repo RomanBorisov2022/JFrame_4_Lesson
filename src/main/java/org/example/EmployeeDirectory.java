cb539ceacdf80228dfac1272055ba9c2b550c775
Roman Borisov <boriso-rom@yandex.ru>
2023-12-22 14:02:32 +0300

    created class Directory and added code

---
+++ src/main/java/org/example/EmployeeDirectory.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class EmployeeDirectory {
+    List<Employee> employees = new ArrayList<>();
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public List<Employee> findByExperience(int requiredExperience) {
+        List<Employee> foundEmployees = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.experience == requiredExperience) {
+                foundEmployees.add(employee);
+            }
+        }
+        return foundEmployees;
+    }
+
+    public List<String> findPhoneNumbersByName(String requiredName) {
+        List<String> phoneNumbers = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.name.equals(requiredName)) {
+                phoneNumbers.add(employee.phoneNumber);
+            }
+        }
+        return phoneNumbers;
+    }
+
+    public Employee findByEmployeeId(int requiredEmployeeId) {
+        for (Employee employee : employees) {
+            if (employee.employeeId == requiredEmployeeId) {
+                return employee;
+            }
+        }
+        return null;
+    }
+}
