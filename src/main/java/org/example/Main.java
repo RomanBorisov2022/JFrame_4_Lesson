80e2b157f22eab8d0055d9975aed9828ae232129
Roman Borisov <boriso-rom@yandex.ru>
2023-12-22 14:04:03 +0300

    last commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmployeeDirectory directory = new EmployeeDirectory();
+
+        // Adding employees
+        directory.addEmployee(new Employee(1, "123-456-7890", "John", 5));
+        directory.addEmployee(new Employee(2, "987-654-3210", "Maria", 3));
+        directory.addEmployee(new Employee(3, "111-222-3333", "Peter", 7));
+
+        // Finding by experience
+        List<Employee> employeesWithExperience5 = directory.findByExperience(5);
+        System.out.println("Employees with 5 years of experience: " + employeesWithExperience5);
+
+        // Finding by name
+        List<String> phoneNumbersOfMaria = directory.findPhoneNumbersByName("Maria");
+        System.out.println("Phone numbers of Maria: " + phoneNumbersOfMaria);
+
+        // Finding by employee ID
+        Employee employeeWithId2 = directory.findByEmployeeId(2);
+        System.out.println("Employee with employee ID 2: " + employeeWithId2);
     }
 }
